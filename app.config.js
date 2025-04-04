e32d25084728aba70fc3f9a9ffd5f1d7790635ae
protovinh <60383292+protovinh@users.noreply.github.com>
2025-04-04 13:24:29 +0200

    Ejection
    Eject from Expo

--- app.config.js
+++ app.config.js
@@ -1,53 +1,52 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-  "expo": {
-    "name": "Cutters",
-    "slug": "Cutters",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "myapp",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true,
-      "config": {
-        "googleMapsApiKey": process.env.GOOGLE_API_KEY
-      }
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"        
+  expo: {
+    name: "Cutters",
+    slug: "Cutters",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_API_KEY,
       },
-      "config": {
-        "googleMaps": {
-          "apiKey": process.env.GOOGLE_API_KEY
-        }
-      }
-
     },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    android: {
+      package: "com.vinvu2000.Cutters",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_API_KEY,
+        },
+      },
     },
-    "plugins": [
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
       "expo-router",
       [
         "expo-splash-screen",
         {
-          "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff"
-        }
-      ]
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
     ],
-    "experiments": {
-      "typedRoutes": true
-    }
-    
-  }
+    experiments: {
+      typedRoutes: true,
+    },
+  },
 };
