2da22651a84ffd8a070040e9d18a1d628bc35fc8
protovinh <60383292+protovinh@users.noreply.github.com>
2025-04-04 22:11:47 +0200

    button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,1 +1,61 @@
-export function Button() {}
+import React from 'react'
+import {
+    Pressable,
+    StyleSheet,
+    Text,
+    TouchableWithoutFeedback,
+} from 'react-native'
+
+interface Props {
+    title: string
+    onPress: () => void
+    disabled?: boolean
+}
+
+const Button = (props: Props) => {
+    if (props.disabled) {
+        return (
+            <Pressable disabled={true} style={styles.disabledContainer}>
+                <Text style={styles.disabledText}>{props.title}</Text>
+            </Pressable>
+        )
+    }
+    return (
+        <Pressable style={styles.buttonContainer} onPress={props.onPress}>
+            <Text style={styles.buttonText}>{props.title}</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#FFDD00',
+        borderRadius: 100,
+        padding: 12,
+        width: '100%',
+    },
+    disabledContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+        borderRadius: 100,
+        padding: 12,
+        width: '100%',
+        borderWidth: 2,
+        borderColor: 'grey',
+    },
+    buttonText: {
+        color: 'black',
+        fontSize: 15,
+        fontWeight: 700,
+    },
+    disabledText: {
+        color: 'grey',
+        fontSize: 15,
+        fontWeight: 700,
+    },
+})
+
+export default Button
--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,9 +1,15 @@
 import { Text, View, StyleSheet } from 'react-native'
+import Button from '@/components/ui/Button'
 
 export default function AboutScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>About screen</Text>
+            <Button
+                title="Press me"
+                onPress={() => {
+                    console.log('Pressed')
+                }}
+            />
         </View>
     )
 }
