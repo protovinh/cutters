90483791381eaa7b603fe3cf592601b029ddbb69
protovinh <60383292+protovinh@users.noreply.github.com>
2025-04-06 23:02:04 +0200

    Updated status bar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import 'expo-dev-client'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import RootNavigator from './src/navigation/RootNavigator'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
     return (
         <SafeAreaProvider style={{ backgroundColor: '#1a1a1a' }}>
+            <StatusBar style="light" />
             <RootNavigator />
         </SafeAreaProvider>
     )
